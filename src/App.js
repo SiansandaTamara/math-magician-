0287ed8d1c4e369cfa95f294707680a14904940f
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-09 17:28:25 +0200

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Calcu from '../components/Calculator';
+import Calcu from './components/Calculator';
 
 import Home from './components/Home';
 import Quote from './components/Quotes';
@@ -10,12 +10,14 @@ class App extends React.PureComponent {
   render() {
     return (
       <>
+
         <div className="display">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="calculator" element={<Calcu />} />
             <Route path="Quotes" element={<Quote />} />
           </Routes>
+
         </div>
       </>
     );
