6f45b7f29aebf519625af7ac78066dbec9e248b3
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-09 20:08:48 +0200

    update

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -4,7 +4,7 @@ class Quote extends React.PureComponent {
   render() {
     return (
       <div className="quote-div">
-        <p className="quote-para">Mathematics as an expression of the human mind reflects the active will, the contemplative reason, and the desire for aesthetic perfection. Its basic elements are logic and intuition, analysis and construction, generality and individuality.</p>
+        <p className="quote-para">Mathematics is not about Numbers, equations, Computations or algorithm; its about understanding. -William Paul Thurston</p>
       </div>
     );
   }
